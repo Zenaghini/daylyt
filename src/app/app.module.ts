676979dad994f0ebc905eefd4f348b9e4753ab2f
Ubuntu <ubuntu@ip-172-31-44-196.ap-southeast-2.compute.internal>
2020-10-08 11:46:54 +0000

    trying to get Amplify authn to work, fails to build because of running out of memory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+/* Add Amplify imports */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+/* Configure Amplify resources */
+Amplify.configure(awsconfig);
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
+    HeroesComponent
   ],
   imports: [
+    AmplifyUIAngularModule,
     BrowserModule,
     FormsModule
   ],
