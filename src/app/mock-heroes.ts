874f00d10527131361f2a49a6e2c99af91470642
Ubuntu <ubuntu@ip-172-31-1-88.ap-southeast-2.compute.internal>
2020-10-12 08:19:16 +0000

    Delete everything. Will recommit backup later with gitignore

--- src/app/mock-heroes.ts
+++
@@ -1,8 +0,0 @@
-import { Hero } from './hero';
-
-export const HEROES: Hero[] = [
-  { id: 11, name: 'Keiichi Tsuchiya' },
-  { id: 12, name: 'Manabu Orido' },
-  { id: 13, name: 'Nobuteru Taniguchi' },
-  { id: 14, name: 'Yasuyuki Kazama' }
-];
