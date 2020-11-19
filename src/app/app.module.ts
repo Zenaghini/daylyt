8102d8997615317040bd570c9fdb064ddf19ae60
Zenaghini DiabloSV <und3rc0v3rbr0ther@gmail.com>
2020-11-19 04:27:30 +0000

    Finally got amplify installed and configured and Cognito working blah blah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+/* Add Amplify imports */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+/* Configure Amplify resources */
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [
@@ -14,7 +22,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AmplifyUIAngularModule /* Add Amplify module */
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+const oauth = {
+  domain: 'project-z-b.auth.ap-southeast-2.amazoncognito.com',
+  scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+  redirectSignIn: 'https://d1h0vim82k77tq.cloudfront.net/',
+  redirectSignOut: 'https://d1h0vim82k77tq.cloudfront.net/',
+  responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+}
+
+Auth.configure({
+  oauth:oauth
+});
+
 if (environment.production) {
   enableProdMode();
 }
